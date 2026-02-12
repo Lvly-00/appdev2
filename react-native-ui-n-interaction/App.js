70a2dff2071b6e225ffd43c398a528385a4f9937
Lovely <128210631+Lvly-00@users.noreply.github.com>
2026-02-12 21:25:53 +0800

    Done with percentage dimensions section

--- react-native-ui-n-interaction/App.js
+++ react-native-ui-n-interaction/App.js
@@ -1,17 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView  } from 'react-native';
+import { SafeAreaView } from 'react-native';
 // import LotsOfStyles from './components/LotsOfStyles'; 
 // import FixedDimensionsBasics from './components/FixedDimensionsBasics';
-import FlexDimensionsBasics from './components/FlexDimensionsBasics';
+// import FlexDimensionsBasics from './components/FlexDimensionsBasics';
+import PercentageDimensionsBasics from './components/PercentageDimensionsBasics';
+
 
 
 
 export default function App() {
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{ flex: 1 }}>
       {/* <LotsOfStyles/> */}
       {/* <FixedDimensionsBasics/> */}
-      <FlexDimensionsBasics/>
+      {/* <FlexDimensionsBasics/> */}
+      <PercentageDimensionsBasics />
+
 
       <StatusBar />
     </SafeAreaView>
