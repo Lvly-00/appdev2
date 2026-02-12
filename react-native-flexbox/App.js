dd516f76c31df6e1c7aa5c785b926bf04aefba5e
Lovely <128210631+Lvly-00@users.noreply.github.com>
2026-02-12 21:55:00 +0800

    Done with align items section

--- react-native-flexbox/App.js
+++ react-native-flexbox/App.js
@@ -3,8 +3,8 @@ import { SafeAreaView } from 'react-native';
 // import Flex from './components/Flex';
 // import FlexDirectionBasics from './components/FlexDirectionBasics';
 // import DirectionLayout from './components/DirectionLayout';
-import JustifyContentBasics from './components/JustifyContentBasics';
-
+// import JustifyContentBasics from './components/JustifyContentBasics';
+import AlignItemsLayout from './components/AlignItemsLayout';
 
 
 
@@ -14,8 +14,8 @@ export default function App() {
       {/* <Flex /> */}
       {/* <FlexDirectionBasics /> */}
       {/* <DirectionLayout /> */}
-      <JustifyContentBasics />
-
+      {/* <JustifyContentBasics /> */}
+      <AlignItemsLayout />
       <StatusBar />
     </SafeAreaView>
   );
