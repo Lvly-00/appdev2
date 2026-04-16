c03029282b7eb2097b6a9f2f00a76f4a091f5e1b
Lovely <128210631+Lvly-00@users.noreply.github.com>
2026-04-17 00:28:57 +0800

    Done with specifying options section

--- react-native-navigation/App.tsx
+++ react-native-navigation/App.tsx
@@ -20,12 +20,22 @@ function DetailsScreen() {
 }
 
 const RootStack = createNativeStackNavigator({
-  initialRouteName: 'Details',
+  initialRouteName: 'Home',
+  screenOptions: {
+    headerStyle: { backgroundColor: 'tomato' },
+  },
   screens: {
-    Home: HomeScreen,
+    Home: {
+      screen: HomeScreen,
+      options: {
+        title: 'Overview',
+      },
+    },
     Details: DetailsScreen,
   },
 });
+
+
 
 const Navigation = createStaticNavigation(RootStack);
 
