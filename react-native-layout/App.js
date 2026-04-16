fc1b31de87c64310cc5e839253aad598b00c404a
Lovely <128210631+Lvly-00@users.noreply.github.com>
2026-04-17 00:23:56 +0800

    Done with Installing packages & dependencies section

--- react-native-layout/App.js
+++ react-native-layout/App.js
@@ -1,17 +1,13 @@
 import React, { useState } from 'react';
 import Login from './screens/Login';
 import Signup from './screens/Signup';
-import Todo from './screens/Todo';
-
 
 export default function App() {
   const [currentScreen, setCurrentScreen] = useState('Login');
 
-  // return currentScreen === 'Login' ? (
-  //   <Login onNavigate={() => setCurrentScreen('Signup')} />
-  // ) : (
-  //   <Signup onNavigate={() => setCurrentScreen('Login')} />
-  // );
-
-  return(<Todo/>);
+  return currentScreen === 'Login' ? (
+    <Login onNavigate={() => setCurrentScreen('Signup')} />
+  ) : (
+    <Signup onNavigate={() => setCurrentScreen('Login')} />
+  );
 }
